a80ae098253fa0c15c7114f304bc795930675406
kang <kanghohyeon@g,ail.com>
2025-09-28 23:23:31 +0900

    0923 프로젝트 세팅

---
+++ src/test/java/com/myapp/valuationproject/ValuationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.valuationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
